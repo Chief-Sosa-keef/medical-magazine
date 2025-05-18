23ed60292a3677a629ff02ef6a3dd6c78a15c7ba
Chief-sosa-keef <modricbest69@gmail.com>
2025-05-18 20:10:33 +0300

    Новая версия проекта: описание изменений

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/universitet-react/", // Название репозитория
+  base: "/universitet-react/",
   build: {
-    outDir: "root", // Папка для сборки проекта
+    outDir: "root",
+    emptyOutDir: true
   }
 })
