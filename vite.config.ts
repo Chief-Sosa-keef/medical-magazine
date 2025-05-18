e8c43e9f5113a91bd2462d04463753652c44d5d0
Chief-sosa-keef <modricbest69@gmail.com>
2025-05-18 21:01:01 +0300

    Rebuild project

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/universitet-react/",
+  base: process.env.NODE_ENV === "production" 
+    ? "/universitet_react/"  // Для GitHub Pages
+    : "/",                   // Для локальной разработки
   build: {
     outDir: "root",
     emptyOutDir: true
