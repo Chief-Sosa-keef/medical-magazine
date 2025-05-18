cfa32a13638d6ef120ecd83cd3687fc7b0f8e55d
Chief-sosa-keef <modricbest69@gmail.com>
2025-05-18 23:05:35 +0300

    Nuclear reset: final fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/universitet_react/", // Должно совпадать с названием репозитория
+  base: "/universitet_react/", // Название репозитория ДОЛЖНО СОВПАДАТЬ!
   build: {
     outDir: "root",
+    assetsDir: "assets",
     emptyOutDir: true
   }
 })
