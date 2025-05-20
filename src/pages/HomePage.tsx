64a23d8c32720ba8d69091fb8452a0de7bc9a1c7
Chief-sosa-keef <modricbest69@gmail.com>
2025-05-20 18:38:30 +0300

    END

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@
 import Banner_cont from '../main/banner/banner-cont'
 import Info_block from '../main/info_block/info_block'
 import Slider from '../main/slider/slider'
+import Button_next from '../main/butt_next/butt_next'
 import ScientificJournalAbout from '../main/last_info_block/lastInfo.block'
 // import Footer from '../footer/footer'
 import NWCDeveloperCard from '../main/NWC/NWCDeveloperCard'
@@ -18,6 +19,7 @@ export default function HomePage() {
       {/* <Head title='..' links={headerLinks} /> */}
       <Banner_cont />
       <Info_block />
+      <Button_next link='#slider'/>
       <Slider />
       <NWCDeveloperCard />
       <ScientificJournalAbout />
